700636ea4bd2de2eb6ba2718579d9e7dbe1084a2
Aris Mandolini <arismandolini@gmail.com>
2020-12-11 01:54:53 +0100

    Test commit

---
+++ Uebung24_Prova.NETCore/Uebung24_Prova.NETCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uebung24_Prova.NETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        length:
+            int length = 10000;
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < length; i++)
+            {
+                goto length;
+            }
+        }
+    }
+}
