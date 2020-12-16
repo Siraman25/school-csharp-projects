285494cdb6f562244fc422a02a36a1213170605d
Aris Mandolini <arismandolini@gmail.com>
2020-12-17 00:48:45 +0100

    Edited Uebung24

--- Uebung24_DatasetArray/Program.cs
+++ Uebung24_DatasetArray/Program.cs
@@ -4,10 +4,10 @@ namespace Uebung24_DatasetArray
 {
     public class Iris
     {
-        public int Data1, Data2, Data3, Data4;
+        public double Data1, Data2, Data3, Data4;
         public string IrisName;
 
-        public Iris(int Data1, int Data2, int Data3, int Data4, string IrisName)
+        public Iris(double Data1, double Data2, double Data3, double Data4, string IrisName)
         {
             this.Data1 = Data1;
             this.Data2 = Data2;
@@ -16,22 +16,22 @@ namespace Uebung24_DatasetArray
             this.IrisName = IrisName;
         }
 
-        public int getData1()
+        public Double getData1()
         {
             return Data1;
         }
 
-        public int getData2()
+        public Double getData2()
         {
             return Data2;
         }
 
-        public int getData3()
+        public Double getData3()
         {
             return Data3;
         }
 
-        public int getData4()
+        public Double getData4()
         {
             return Data4;
         }
@@ -48,10 +48,41 @@ namespace Uebung24_DatasetArray
     }
     class Program
     {
+        static string[] ArrayReader(string filePath, ref int counter)
+        {
+            System.IO.StreamReader readData = new System.IO.StreamReader(filePath);
+            string[] output, lines;
+            string text;
+            text = readData.ReadToEnd();
+            lines = text.Split(Environment.NewLine);
+            output = lines[counter].Split(",");
+
+            if (counter == lines.Length -1) {
+                counter = -1;
+            }
+            else {
+                counter++;
+            }
+
+            return (output);
+        }
         static void Main(string[] args)
         {
-            Iris prova123 = new Iris(55, 44, 33, 22, "Ciao");
-            System.Console.WriteLine(prova123.Stringed());
+            int counter = 0;
+            while (counter != -1)
+            {
+                string[] ArrayCtr = ArrayReader("/home/aris/C#/Uebung24_DatasetArray/Datasets/iris.data", ref counter);
+                double data1 = Convert.ToDouble(ArrayCtr[0]);
+                double data2 = Convert.ToDouble(ArrayCtr[1]);
+                double data3 = Convert.ToDouble(ArrayCtr[2]);
+                double data4 = Convert.ToDouble(ArrayCtr[3]);
+                string data5 = ArrayCtr[4];
+
+                Iris prova123 = new Iris(data1, data2, data3, data4, data5);
+                System.Console.WriteLine(prova123.Stringed());
+            }
+            System.Console.WriteLine("-----------");
+            System.Console.WriteLine("END REACHED");
         }
     }
 }
