b4684fe9ff27ebbebf991babf6df18fa8701075a
Aris Mandolini <arismandolini@gmail.com>
2020-12-17 16:24:44 +0100

    Prova

---
+++ Uebung24_PROVADOTNETCORERIDER/Uebung24_PROVADOTNETCORERIDER/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Uebung24_PROVADOTNETCORERIDER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CIAO!");
+            Console.WriteLine("Prova123");
+        }
+    }
+}
